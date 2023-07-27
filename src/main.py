570d419e430cbe84d52683c61afb97d61e2ffa2d
Артур <informal201144@gmai;.com>
2023-07-27 23:57:09 +0500

    added fix

--- src/main.py
+++ src/main.py
@@ -7,11 +7,11 @@ DB_NAME = 'data'
 
 
 def main():
-    companies = ['195398', '4181', '4484134', '4496', '78638', '4019151', '80660', '6082361', '2489601',
+    companies_id = ['195398', '4181', '4484134', '4496', '78638', '4019151', '80660', '6082361', '2489601',
                  '740349']  # id компаний
-    department = get_companies(companies)  # Получение списка работодателей и их вакансий с платформы HeadHunter
+    companies = get_companies(companies_id)  # Получение списка работодателей и их вакансий с платформы HeadHunter
     create_database(DB_NAME, PARAMS)  # Создание базы данных
-    save_data_to_database(department, DB_NAME, PARAMS)  # Добавление информаций по вакансиям и компаниям в таблицы"
+    save_data_to_database(companies, DB_NAME, PARAMS)  # Добавление информаций по вакансиям и компаниям в таблицы"
 
 
 db = DBManager(DB_NAME)
